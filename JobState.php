48b0887d87862405d97574586c7046035c9cdb38
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2019-09-18 09:54:10 +0430

    Add JobState class to determine different status of job

---
+++ JobState.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * This file is part of Pluf Framework, a simple PHP Application Framework.
+ * Copyright (C) 2010-2020 Phoinex Scholars Co. (http://dpq.co.ir)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace Pluf\Jms;
+
+class JobState {
+    public const STATUS_INIT = "init";
+    public const STATUS_WAIT = "ready";
+    public const STATUS_IN_PROGRESS = "in-progress";
+    public const STATUS_STOPPED = "stopped";
+    public const STATUS_ERROR = "error";
+    public const STATUS_COMPLETE = "done";
+    
+}
